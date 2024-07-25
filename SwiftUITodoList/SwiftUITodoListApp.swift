d84273d6c382598be62077d9a52c6ad4e661f9bf
Chechobuitrago <buitrago12.09@gmail.com>
2024-07-25 16:36:39 -0500

    Initial Commit

---
+++ SwiftUITodoList/SwiftUITodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoListApp.swift
+//  SwiftUITodoList
+//
+//  Created by Sergio Buitrago on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
